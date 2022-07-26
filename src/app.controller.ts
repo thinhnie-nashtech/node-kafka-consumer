a21a972e370596f46b461cda156fb9b741886ad4
Thinh Nguyen Nie Duc <thinh.nguyennieduc@nashtechglobal.com>
2022-07-26 11:16:38 +0700

    consumer worked

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('hot-news') // Our topic name
+  getHello(@Payload() message) {
+    console.log(message);
+    return 'Hello World';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['ec2-13-229-128-200.ap-southeast-1.compute.amazonaws.com:9092'],
+      },
+      consumer: {
+          groupId: 'hot-news-consumer',
+      }
+    }
+  });
+
+  app.listen().then(() => console.log('Kafka consumer service is listening!'));
 }
+
 bootstrap();
