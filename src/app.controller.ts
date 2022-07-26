64e3dfd2bd36c68e1ba05b97630c9d17fd48ce8f
Thinh Nguyen Nie Duc <thinh.nguyennieduc@nashtechglobal.com>
2022-07-26 13:53:58 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { MessageType } from './models/message-type.enum';
+import { IMessage } from './models/messge.interface';
 import { CreateNewsDto } from './news/dtos/create-news.dto';
 import { NewsService } from './news/news.service';
 
@@ -9,9 +11,22 @@ export class AppController {
   constructor(private readonly appService: AppService, private readonly newsService: NewsService) {}
 
   @MessagePattern('hot-news') // Our topic name
-  getHello(@Payload() message: CreateNewsDto) {
+  saveNews(@Payload() message: IMessage) {
     console.log(message);
-    this.newsService.create(message)
-    return 'Hello World';
+    let newsToCreate: CreateNewsDto;
+    if(message.type == MessageType.CREATE_NEWS)
+      newsToCreate = message.msg as CreateNewsDto;
+    this.newsService.create(newsToCreate)
+    return 'Save ok';
+  }
+
+  @MessagePattern('hot-news') // Our topic name
+  deleteNew(@Payload() message: IMessage) {
+    console.log(message);
+    let newsToCreate: CreateNewsDto;
+    if(message.type == MessageType.DELETE_NEWS)
+      newsToCreate = message.msg as CreateNewsDto;
+    this.newsService.create(newsToCreate)
+    return 'Save ok';
   }
 }
